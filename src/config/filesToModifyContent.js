b0839185448c160e275a361d5aaf4ed84dd95b7b
Nadia Dillon <dillon.nadia@gmail.com>
2018-02-27 11:20:21 +0000

    feat: add option to set custom display name

--- src/config/filesToModifyContent.js
+++ src/config/filesToModifyContent.js
@@ -1,55 +1,58 @@
 // nS - No Space
 // lC - Lowercase
 
-export function filesToModifyContent(currentAppName, newName, projectName) {
-  const nS_CurrentAppName = currentAppName.replace(/\s/g, '');
-  const nS_NewName = newName.replace(/\s/g, '');
-  const lC_Ns_CurrentAppName = nS_CurrentAppName.toLowerCase();
-  const lC_Ns_NewName = nS_NewName.toLowerCase();
+export function filesToModifyContent(currentAppName, newName, displayName, projectName) {
+	const nS_CurrentAppName = currentAppName.replace(/\s/g, '');
+	const nS_NewName = newName.replace(/\s/g, '');
+	const lC_Ns_CurrentAppName = nS_CurrentAppName.toLowerCase();
+	const lC_Ns_NewName = nS_NewName.toLowerCase();
+	const nS_DisplayName = displayName.replace(/\s/g, '');
 
-  return [
-    {
-      regex: `<string name="app_name">${currentAppName}</string>`,
-      replacement: `<string name="app_name">${newName}</string>`,
-      paths: ['./android/app/src/main/res/values/strings.xml']
-    },
-    {
-      regex: nS_CurrentAppName,
-      replacement: nS_NewName,
-      paths: [
-        './index.android.js',
-        './index.ios.js',
-        `./ios/${nS_NewName}.xcodeproj/project.pbxproj`,
-        `./ios/${nS_NewName}.xcworkspace/contents.xcworkspacedata`,
-        `./ios/${nS_NewName}.xcodeproj/xcshareddata/xcschemes/${nS_NewName}-tvOS.xcscheme`,
-        `./ios/${nS_NewName}.xcodeproj/xcshareddata/xcschemes/${nS_NewName}.xcscheme`,
-        `./ios/${nS_NewName}/AppDelegate.m`,
-        './android/settings.gradle',
-        `./ios/${nS_NewName}Tests/${nS_NewName}Tests.m`,
-        './ios/build/info.plist',
-        './ios/Podfile',
-        './app.json'
-      ]
-    },
-    {
-      regex: `text="${currentAppName}"`,
-      replacement: `text="${newName}"`,
-      paths: [`./ios/${nS_NewName}/Base.lproj/LaunchScreen.xib`]
-    },
-    {
-      regex: currentAppName,
-      replacement: newName,
-      paths: [`./ios/${nS_NewName}/Info.plist`]
-    },
-    {
-      regex: `"name": "${nS_CurrentAppName}"`,
-      replacement: `"name": "${nS_NewName}"`,
-      paths: ['./package.json']
-    },
-    {
-      regex: `"displayName": "${currentAppName}"`,
-      replacement: `"displayName": "${newName}"`,
-      paths: ['./app.json']
-    }
-  ];
+	const nameUsedForDisplay = nS_DisplayName || nS_NewName;
+
+	return [
+		{
+			regex: `<string name="app_name">${currentAppName}</string>`,
+			replacement: `<string name="app_name">${nameUsedForDisplay}</string>`,
+			paths: ['./android/app/src/main/res/values/strings.xml']
+		},
+		{
+			regex: nS_CurrentAppName,
+			replacement: nS_NewName,
+			paths: [
+				'./index.android.js',
+				'./index.ios.js',
+				`./ios/${nS_NewName}.xcodeproj/project.pbxproj`,
+				`./ios/${nS_NewName}.xcworkspace/contents.xcworkspacedata`,
+				`./ios/${nS_NewName}.xcodeproj/xcshareddata/xcschemes/${nS_NewName}-tvOS.xcscheme`,
+				`./ios/${nS_NewName}.xcodeproj/xcshareddata/xcschemes/${nS_NewName}.xcscheme`,
+				`./ios/${nS_NewName}/AppDelegate.m`,
+				'./android/settings.gradle',
+				`./ios/${nS_NewName}Tests/${nS_NewName}Tests.m`,
+				'./ios/build/info.plist',
+				'./ios/Podfile',
+				'./app.json'
+			]
+		},
+		{
+			regex: `text="${currentAppName}"`,
+			replacement: `text="${newName}"`,
+			paths: [`./ios/${nS_NewName}/Base.lproj/LaunchScreen.xib`]
+		},
+		{
+			regex: currentAppName,
+			replacement: nameUsedForDisplay,
+			paths: [`./ios/${nS_NewName}/Info.plist`]
+		},
+		{
+			regex: `"name": "${nS_CurrentAppName}"`,
+			replacement: `"name": "${nS_NewName}"`,
+			paths: ['./package.json']
+		},
+		{
+			regex: `"displayName": "${currentAppName}"`,
+			replacement: `"displayName": "${newName}"`,
+			paths: ['./app.json']
+		}
+	];
 }
